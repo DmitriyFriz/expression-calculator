facaf2f767633348eea8d20509d740810dadda83
DmitriyFriz <dmitriy.bulavko.friz44@gmail.com>
2020-03-22 23:46:37 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -4,7 +4,39 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+  let arr = expr.match(/([+*-/)(]{1}|\d+)/g);
+
+  for (let i = 0; i < arr.length; i++ ){
+    if (arr[i] == '/' && arr[i+1] == '0') throw new Error('TypeError: Division by zero.');
+  }
+
+  for (let i = 0; i < arr.length; i++ ){
+    if (arr[i] == '*'){
+      let result = Number(arr[i-1]) * Number(arr[i+1]);
+      arr.splice(i-1, 3, result);
+      i--;
+    }
+    if (arr[i] == '/')  {
+      let result = Number(arr[i-1]) / Number(arr[i+1]);
+      arr.splice(i-1, 3, result);
+      i--;
+    }
+  }
+
+  for (let i = 0; i < arr.length; i++ ){
+    if (arr[i] == '+'){
+      let result = Number(arr[i-1]) + Number(arr[i+1]);
+      arr.splice(i-1, 3, result);
+      i--;
+    }
+    if (arr[i] == '-')  {
+      let result = Number(arr[i-1]) - Number(arr[i+1]);
+      arr.splice(i-1, 3, result);
+      i--;
+    }
+  }
+
+  return arr[0];
 }
 
 module.exports = {
